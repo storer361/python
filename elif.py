09ebbe2b730bce2538f796c2b8b4a1719542cdb1
storer361 <muneeburrehmaan304@gmail.com>
2025-08-31 04:13:46 -0700

    loops

---
+++ elif.py
@@ -0,0 +1,7 @@
+E=int(input("Enter a number:"))
+if E>0:
+    print("The number is positive")
+elif E<0:
+    print("The number is negative")
+else:
+    print("The number is 0")
---
+++ is elfa.py
@@ -0,0 +1,5 @@
+G=input("enter any character:")
+if G.isalpha():
+    print(f"{G}  is an alphabet")
+else:
+    print(f"{G}  is not an alphabet")
