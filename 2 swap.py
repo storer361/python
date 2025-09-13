9c22b23e7b6b0bd75f4cb025d60232271d27757a
storer361 <muneeburrehmaan304@gmail.com>
2025-09-13 13:43:28 +0300

    pattern.py

---
+++ 2 swap.py
@@ -0,0 +1,9 @@
+a=65
+b=19
+print("before swaping a is 65")
+print("before swaping b is 19")
+c=a
+a=b
+b=c
+print("after swaping a is ",a)
+print("after swaping b is ",b)
---
+++ pattern.py
@@ -0,0 +1,6 @@
+print("half pyramid pattern of stars(a):")
+n=int(input("enter the number of rows :"))
+for i in range (n):
+    for j in range (i+1):
+        print("a",end="")
+    print()
